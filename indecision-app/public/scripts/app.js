5124cdcfe051954b11673c29af79c3d07cd969c0
Marcos Schead <wssmarcos@gmail.com>
2017-12-14 10:40:31 -0200

    Section 3: Setting up Babel

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,13 +1,13 @@
+'use strict';
+
 console.log('App.js is running!');
 
 // JSX - JavaScript XML
-// var template = <p>This is JSX from app.js!</p>;
 var template = React.createElement(
-  "h1",
+  'h1',
   null,
-  "Something new"
+  'Indecision App'
 );
-
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
