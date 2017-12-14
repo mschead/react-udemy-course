b931406bcdd7385b9bb6c271740b5c1211a7dcf3
Marcos Schead <wssmarcos@gmail.com>
2017-12-14 10:17:46 -0200

    Section 3: Hello React

---
+++ indecision-app/public/scripts/app.js
@@ -0,0 +1,13 @@
+console.log('App.js is running!');
+
+// JSX - JavaScript XML
+// var template = <p>This is JSX from app.js!</p>;
+var template = React.createElement(
+  "h1",
+  null,
+  "Something new"
+);
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
