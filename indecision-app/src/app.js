d52c35a2dba681f8af9f6e12368283ce7766d06a
Marcos Schead <wssmarcos@gmail.com>
2018-01-11 15:15:52 -0200

    Section 6: ES6 class properties

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -2,8 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
-// Moving the component code
-// Setup import/export
-// Move the imports to that new files (and setup an import here)
-
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+
+class OldSyntax {
+    
+    constructor() {
+			this.name = 'Mike';
+			this.getGreeting = this.getGreeting.bind(this);
+    }
+
+    getGreeting() {
+      return `Hi. My name is ${this.name}.`;
+    }
+
+}
+
+const oldSyntax = new OldSyntax();
+const getGreeting = oldSyntax.getGreeting; 
+console.log(getGreeting());
+
+// ---------
+
+class NewSyntax {
+	
+	name = 'Jen';
+	
+	getGreeting = () => {
+		return `Hi. My name is ${this.name}.`;
+	}
+
+}
+const newSyntax = new NewSyntax();
+const newGetGreeting = newSyntax.getGreeting;
+console.log(newGetGreeting());
--- indecision-app/src/components/IndecisionApp.js
+++ indecision-app/src/components/IndecisionApp.js
@@ -6,20 +6,47 @@ import Options from './Options';
 import Header from './Header';
 import Action from './Action';
 
-export default class IndecisionApp extends React.Component {
-	
-	constructor(props) {
-		super(props);
-		this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-		this.handlePick = this.handlePick.bind(this);
-		this.handleAddOption = this.handleAddOption.bind(this);
-		this.handleDeleteOption = this.handleDeleteOption.bind(this);
+// pull the state out of the constructor
+// convert all 4 event handlers to class properties (arrow functions)
+// delete the constructor completely
+// start with classe properties and end with methods
 
-		this.state = {
-			options: []
-		};
-	}
-	
+export default class IndecisionApp extends React.Component {
+
+	state = {
+		options: []
+	};
+
+	handleDeleteOptions = () => {
+		this.setState(() => ({options: []}));
+	};
+
+	handleDeleteOption = (optionToRemove) => {
+		this.setState((prevState) => ({
+			options: prevState.options.filter((option) => option !== optionToRemove)
+		}));
+	};
+
+	handlePick = () => {
+		const randomNum = Math.floor(Math.random() * this.state.options.length);
+		const option = this.state.options[randomNum];
+		alert(option);
+	};
+
+	handleAddOption = (option) => {
+		
+		if (!option) {
+			return 'Enter valid value to add item';
+		} else if (this.state.options.indexOf(option) > -1) {
+			return 'This option already exists';
+		}
+
+		this.setState((prevState) => (
+			{options: prevState.options.concat(option)
+		}));
+
+	};
+
 	componentDidMount() {
 		try {
 			const json = localStorage.getItem('options');
@@ -43,36 +70,6 @@ export default class IndecisionApp extends React.Component {
 
 	componentWillUnmount() {
 		console.log('componentWillUnmount!');
-	}
-
-	handleDeleteOptions() {
-		this.setState(() => ({options: []}));
-	}
-
-	handleDeleteOption(optionToRemove) {
-		this.setState((prevState) => ({
-			options: prevState.options.filter((option) => option !== optionToRemove)
-		}));
-	}
-
-	handlePick() {
-		const randomNum = Math.floor(Math.random() * this.state.options.length);
-		const option = this.state.options[randomNum];
-		alert(option);
-	}
-
-	handleAddOption(option) {
-		
-		if (!option) {
-			return 'Enter valid value to add item';
-		} else if (this.state.options.indexOf(option) > -1) {
-			return 'This option already exists';
-		}
-
-		this.setState((prevState) => (
-			{options: prevState.options.concat(option)
-		}));
-
 	}
 
 	render() {
