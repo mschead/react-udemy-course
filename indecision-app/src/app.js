a3705affdb0fdfb18099008f3d63629b76dc02a5
Marcos Schead <wssmarcos@gmail.com>
2018-01-11 18:55:13 -0200

    Section 7: Passing Children to Component

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -2,36 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+// could create an attribute and use props to get it.
+const Layout = (props) => {
+	return (
+		<div>
+			<p>header</p>
+			{props.children}
+			<p>footer</p>
+		</div>
+	);
+};
 
-class OldSyntax {
-    
-    constructor() {
-			this.name = 'Mike';
-			this.getGreeting = this.getGreeting.bind(this);
-    }
-
-    getGreeting() {
-      return `Hi. My name is ${this.name}.`;
-    }
-
-}
-
-const oldSyntax = new OldSyntax();
-const getGreeting = oldSyntax.getGreeting; 
-console.log(getGreeting());
-
-// ---------
-
-class NewSyntax {
-	
-	name = 'Jen';
-	
-	getGreeting = () => {
-		return `Hi. My name is ${this.name}.`;
-	}
-
-}
-const newSyntax = new NewSyntax();
-const newGetGreeting = newSyntax.getGreeting;
-console.log(newGetGreeting());
+ReactDOM.render((
+	<Layout>
+		<div>
+			<h1>Page Title</h1>
+			<p>This is my page</p>
+		</div>
+	</Layout>
+), document.getElementById('app'));
