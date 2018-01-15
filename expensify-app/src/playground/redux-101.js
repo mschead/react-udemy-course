9c42e514cd1f4cb0456e49ad7deb09f7ef580ce2
Marcos Schead <wssmarcos@gmail.com>
2018-01-15 16:13:58 -0200

    Section 10: Dispatching Actions

--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -1,11 +1,45 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count: 0 }) => {
-  return state;
+const store = createStore((state = { count: 0 }, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      };
+    case 'DECREMENT':
+      return {
+        count: state.count - 1
+      };
+    case 'RESET':
+      return {
+        count: 0
+      };
+    default:
+      return state;
+  }
 });
 
 console.log(store.getState());
 
 // Actions
+
 // I'd like to increment the count
+store.dispatch({
+  type: 'INCREMENT'
+});
+
+store.dispatch({
+  type: 'INCREMENT'
+});
+
 // I'd like to reset the count to zero
+store.dispatch({
+  type: 'RESET'
+});
+
+// I'd like to decrement the count
+store.dispatch({
+  type: 'DECREMENT'
+});
+
+console.log(store.getState());
