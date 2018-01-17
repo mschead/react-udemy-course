bfade785187ce1e2d4cc4b3be0d3f9d11da8c475
Marcos Schead <wssmarcos@gmail.com>
2018-01-17 15:54:39 -0200

    Section 11: Organizing Redux

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisiblesExpenses from './selectors/expenses';
+
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
+
+const store = configureStore();
+
+// addExpense -> Water bill
+// addExpense -> Gas bill
+// setTextFilter -> bill (2 items) -> water (1 item)
+// getVisibleExpenses -> print visibles ones to screen
+store.dispatch(addExpense({ description: 'Water bill'}));
+store.dispatch(addExpense({ description: 'Gas bill'}));
+store.dispatch(setTextFilter('bill'));
+store.dispatch(setTextFilter('water'));
+
+const state = store.getState();
+const visibleExpenses = getVisiblesExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
