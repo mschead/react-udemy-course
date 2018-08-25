c6ee8977898cef1cf25db6a16ef4ab4379df88b0
Marcos Schead <wssmarcos@gmail.com>
2018-08-25 15:00:32 -0300

    Section 14: Removing Data from Firebase

--- expensify-app/src/firebase/firebase.js
+++ expensify-app/src/firebase/firebase.js
@@ -27,16 +27,10 @@ database.ref().set({
     console.log('This failed.', e);
 });
 
-// database.ref().set('This is my data.');
-
-// database.ref('age').set(28);
-// database.ref('location/city').set('Curitiba');
-
-database.ref('attributes').set({
-    height: 170,
-    weight: 60
-}).then(() => {
-    console.log('Second set call worked.')
-}).catch((e) => {
-    console.log('Things didn\'t worked for the second error', e);
-});
+// database.ref()
+//     .remove()
+//     .then(() => {
+//         console.log('Data was removed.');
+//     }).catch((e) => {
+//         console.log('Did not remove data.', e);
+//     });
