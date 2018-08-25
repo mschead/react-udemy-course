3b1ea93172485b5e34d8ad8811ab22ec41319e99
Marcos Schead <wssmarcos@gmail.com>
2018-08-25 18:16:10 -0300

    Section 14: Array Data in Firebase - Part II

--- expensify-app/src/firebase/firebase.js
+++ expensify-app/src/firebase/firebase.js
@@ -13,24 +13,51 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
+// child_removed
+database.ref('expenses').on('child_removed', (snapshot) => {
+    console.log(snapshot.key, snapshot.val());
+});
+
+// child_changed
+database.ref('expenses').on('child_changed', (snapshot) => {
+    console.log(snapshot.key, snapshot.val());
+});
+
+// child_added
+database.ref('expenses').on('child_added', (snapshot) => {
+    console.log(snapshot.key, snapshot.val());
+});
+
+// database.ref('expenses')
+//   .once('value')
+//   .then((snapshot) => {
+//     const expenses = [];
+//     snapshot.forEach((childSnapshot) => {
+//         expenses.push({
+//             id: childSnapshot.key,
+//             ...childSnapshot.val()
+//         });
+//     });
+
+//     console.log(expenses);
+//   });
+
+// database.ref('expenses').on('value', (snapshot) => {
+//     const expenses = [];
+//     snapshot.forEach((childSnapshot) => {
+//         expenses.push({
+//             id: childSnapshot.key,
+//             ...childSnapshot.val()
+//         });
+//     });
+
+//     console.log(expenses);
+// });
+
 database.ref('expenses').push({
     description: 'Rent',
     note: '',
     amount: 5900,
-    createdAt: 976123497834
-});
-
-database.ref('expenses').push({
-    description: 'Phone Bill',
-    note: '',
-    amount: 109500,
-    createdAt: 976123497834
-});
-
-database.ref('expenses').push({
-    description: 'Food',
-    note: '',
-    amount: 1200,
     createdAt: 976123497834
 });
 
