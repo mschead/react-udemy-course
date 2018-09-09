6a9aefc31b04be8588d610c182b926b56bfd694e
Marcos Schead <wssmarcos@gmail.com>
2018-09-09 17:28:00 -0300

    Section 16: Private Only Routes

--- expensify-app/src/routers/AppRouter.js
+++ expensify-app/src/routers/AppRouter.js
@@ -6,9 +6,8 @@ import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
-import Header from '../components/Header';
 import LoginPage from '../components/LoginPage';
-
+import PrivateRoute from './PrivateRoute';
 // Create 6 new files for the 6 components
 // setup imports, components, default export
 // import into AppRouter so they can be used
@@ -18,12 +17,11 @@ export const history = createHistory();
 const AppRouter = () => (
   <Router history={history}>
     <div>
-      <Header />
       <Switch>
         <Route path="/" component={LoginPage} exact={true}/>
-        <Route path="/dashboard" component={ExpenseDashboardPage}/>
-        <Route path="/create" component={AddExpensePage}/>
-        <Route path="/edit/:id" component={EditExpensePage}/>
+        <PrivateRoute path="/dashboard" component={ExpenseDashboardPage}/>
+        <PrivateRoute path="/create" component={AddExpensePage}/>
+        <PrivateRoute path="/edit/:id" component={EditExpensePage}/>
         <Route path="/help" component={HelpPage}/>
         <Route component={NotFoundPage}/>
       </Switch>
